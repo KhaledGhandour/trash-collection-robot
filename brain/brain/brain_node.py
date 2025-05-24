d625883c17a286ba52de8f3c5b2b90313da626fb
Khaled Ghandour <khaled.essam.200230@gmail.com>
2025-05-24 21:15:14 +0300

    trash collection robot

---
+++ brain/brain/brain_node.py
@@ -0,0 +1,226 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from custom_interface.srv import ComponentStatus
+
+
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
+
+
+class brain_node(Node):
+
+    def __init__(self):
+        
+        super().__init__("brain_node")
+        self.callback_group = ReentrantCallbackGroup()
+
+
+
+        self.detection_subscriber=self.create_subscription(String,"trash_detection",self.trash_detection_callback,10,callback_group=self.callback_group)
+
+
+
+        self.component_status_client=self.create_client(ComponentStatus,"comonpont_status")
+        while not self.component_status_client.wait_for_service(timeout_sec=1.0):
+            if not rclpy.ok():
+                self.get_logger().error('Interruped while waiting for the server.')
+                return
+            else:
+                self.get_logger().info('Server not available, waiting again...')
+
+        self.request=ComponentStatus.Request()
+
+        self.componentStatusTimer=self.create_timer(5,self.send_component_status_request,callback_group=self.callback_group)
+
+
+    
+
+    def trash_detection_callback(self,msg):
+       self.get_logger().info('I heard: "%s"' % msg.data)
+       message = msg.data
+       if "trash" in message:
+           self.get_logger().info("Sending move request to actuator")
+       else:
+           self.get_logger().info("Sending no request to actuator")
+
+
+
+
+
+
+
+
+
+    async def send_component_status_request(self):
+      
+
+      self.request.component = "camera"
+      self.future = self.component_status_client.call_async(self.request)
+      result = await self.future
+      self.get_logger().info(f"Service response is:{result.status}")
+
+
+
+        
+
+    
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+def main(args=None):
+
+    rclpy.init(args=args)
+    brainNode = brain_node()
+
+    executor = MultiThreadedExecutor()
+
+    rclpy.spin(brainNode,executor)
+    brain_node.destroy_node()
+    brain_node.destroy_timer()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import rclpy
+# from rclpy.node import Node
+# from std_msgs.msg import String
+# from custom_interfaces.srv import ComponentStatus
+
+# from rclpy.callback_groups import ReentrantCallbackGroup
+# from rclpy.executors import MultiThreadedExecutor
+
+
+# class BrainNode(Node):
+
+#   def __init__(self):
+#     super().__init__('brain_node')
+    
+#     self.trashDetectionTopic = self.declare_parameter(
+#         "trashDetectionTopic",
+#         "None"
+#     )
+#     self.componentStatusService = self.declare_parameter(
+#         "componentStatusService",
+#         "None"
+#     )
+
+#     self.get_parameter_values()
+#     self.callback_group = ReentrantCallbackGroup()
+
+#     self.trashDetectionSubscriber = self.create_subscription(
+#         String,
+#         self.trashDetectionTopic,
+#         self.trash_detection_callback,
+#         10,
+#         callback_group=self.callback_group
+#     )
+
+#     self.componentStatusServiceTimer = self.create_timer(
+#         5,
+#         self.send_component_status_request,
+#         callback_group=self.callback_group
+#     )
+
+#     self.componentStatusClient = self.create_client(
+#         ComponentStatus, self.componentStatusService)
+#     while not self.componentStatusClient.wait_for_service(timeout_sec=1.0):
+#       self.get_logger().info('service not available, waiting again...')
+#     self.request = ComponentStatus.Request()
+
+#   def get_parameter_values(self):
+#     self.trashDetectionTopic = self.get_parameter(
+#         'trashDetectionTopic').get_parameter_value().string_value
+#     self.componentStatusService = self.get_parameter(
+#         'componentStatusService').get_parameter_value().string_value
+
+#   def trash_detection_callback(self, msg):
+#     self.get_logger().info('I heard: "%s"' % msg.data)
+#     message = msg.data
+#     if "trash" in message:
+#       self.get_logger().info("Sending move request to actuator")
+#     else:
+#       self.get_logger().info("Sending no request to actuator")
+
+#   async def send_component_status_request(self):
+#     self.request.component = "camera"
+#     self.future = self.componentStatusClient.call_async(self.request)
+#     result = await self.future
+#     self.get_logger().info(f"Service response is:{result.status}")
+
+
+# def main(args=None):
+#   rclpy.init(args=args)
+
+#   brainNode = BrainNode()
+
+#   executor = MultiThreadedExecutor()
+#   rclpy.spin(brainNode, executor)
+#   brainNode.destroy_node()
+#   rclpy.shutdown()
+
+
+# if __name__ == '__main__':
+#   main()
---
+++ preception/preception/preception_node.py
@@ -0,0 +1,257 @@
+import rclpy
+from rclpy.node import Node
+
+
+from std_msgs.msg import String
+from custom_interface.srv import ComponentStatus
+
+
+class preception_node(Node):
+    def __init__(self):
+        super().__init__("preception_node")
+
+        self.camera_subscriner=self.create_subscription(String,'camera',self.camera_callback,10)
+        self.trash_detection_publisher=self.create_publisher(String,"trash_detection",10)
+        self.component_status_service=self.create_service(ComponentStatus,'comonpont_status',self.handle_component_status)
+
+
+    
+
+    def camera_callback(self,message):
+
+        msg=String()
+
+        if (message.data == "trash"):
+            msg.data = "I see Trash"
+        
+        else:
+            msg.data = " iI can not see any thing"
+
+        
+        self.trash_detection_publisher.publish(msg)
+        self.get_logger().info('publishing : %s'%msg.data)
+
+
+
+
+    
+    def handle_component_status(self, request, response):
+      self.get_logger().info("Server called!")
+      if request.component == "camera":
+       response.status = "Camera Battery is at 100%"
+      else:
+       response.status = "Error: Component unavailable"
+
+      return response
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+
+       
+    rclpy.spin(preception_node())
+
+    """
+    Destroy the node explicitly
+    (optional - otherwise it will be done automatically
+    when the garbage collector destroys the node object)
+    """
+
+    
+    preception_node.destroy_node()
+    rclpy.shutdown()
+
+
+
+
+
+if __name__=='__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import rclpy
+# from rclpy.node import Node
+
+# from std_msgs.msg import String
+# from custom_interfaces.srv import ComponentStatus
+
+
+# class PerceptionNode(Node):
+
+#   def __init__(self):
+#     super().__init__('perception_node')
+
+#     self.cameraTopic = self.declare_parameter(
+#         "cameraTopic",
+#         "None"
+#     )
+#     self.trashDetectionTopic = self.declare_parameter(
+#         "trashDetectionTopic",
+#         "None"
+#     )
+#     self.componentStatusService = self.declare_parameter(
+#         "componentStatusService",
+#         "None"
+#     )
+
+#     self.get_parameter_values()
+
+#     self.cameraSubscriber = self.create_subscription(
+#         String,
+#         self.cameraTopic,
+#         self.camera_callback,
+#         10
+#     )
+
+#     self.trashDetectionPublisher = self.create_publisher(
+#         String,
+#         self.trashDetectionTopic,
+#         10
+#     )
+
+#     self.componentStatusService = self.create_service(
+#         ComponentStatus,
+#         self.componentStatusService,
+#         self.handle_component_status
+#     )
+
+#   def get_parameter_values(self):
+#     self.cameraTopic = self.get_parameter(
+#         'cameraTopic').get_parameter_value().string_value
+#     self.trashDetectionTopic = self.get_parameter(
+#         'trashDetectionTopic').get_parameter_value().string_value
+#     self.componentStatusService = self.get_parameter(
+#         'componentStatusService').get_parameter_value().string_value
+
+#   def camera_callback(self, message):
+#     msg = String()
+#     """
+#     In practical cases, we process images to understand if trash is present
+#     """
+#     if (message.data == "trash"):
+#       msg.data = "I see trash"
+#     else:
+#       msg.data = "I see nothing"
+#     self.trashDetectionPublisher.publish(msg)
+#     self.get_logger().info('Publishing: "%s"' % msg.data)
+
+#   def handle_component_status(self, request, response):
+#     self.get_logger().info("Server called!")
+#     if request.component == "camera":
+#       response.status = "Camera Battery is at 100%"
+#     else:
+#       response.status = "Error: Component unavailable"
+
+#     return response
+
+
+# def main(args=None):
+#   rclpy.init(args=args)
+
+#   perceptionNode = PerceptionNode()
+
+#   rclpy.spin(perceptionNode)
+
+#   # Destroy the node explicitly
+#   # (optional - otherwise it will be done automatically
+#   # when the garbage collector destroys the node object)
+#   perceptionNode.destroy_node()
+#   rclpy.shutdown()
+
+
+# if __name__ == '__main__':
+#   main()
